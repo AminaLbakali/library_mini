64fa9aef66e291d6d8cc32093756e7b892179b7b
zakaria <bouzbitazakaria5@.com>
2024-05-10 23:32:46 +0100

    server connection + env config

---
+++ backend/api_livre/index.js
@@ -0,0 +1,23 @@
+import express from "express"
+import dotenv from 'dotenv'
+import mongoose from "mongoose";
+
+const app = express();
+dotenv.config();
+app.use(express.json())
+
+const port = process.env.PORT;
+const mongoDB = process.env.MONGODB
+
+mongoose.connect(`${mongoDB}/livresDB`).then(()=>{console.log('connected to mongodb')}).catch(()=>{
+   console('error connection to mongodb', error)
+})
+
+
+app.listen(port , ()=>{
+  try{
+    console.log('server running on port : ' + port)
+  }catch(e){
+    console.log('error running server ' + e)
+  }
+})
