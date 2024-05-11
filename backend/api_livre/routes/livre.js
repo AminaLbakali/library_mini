5db3db70ab6bf1de582bb7b3a1156f520b5826bd
zakaria <bouzbitazakaria5@.com>
2024-05-11 10:09:52 +0100

    crud livre

--- backend/api_livre/routes/livre.js
+++ backend/api_livre/routes/livre.js
@@ -14,7 +14,39 @@ router.get("/", async (req, res) => {
 
 router.post("/", async (req, res) => {
   const newBook = req.body;
-  livre.create(newBook).then((book) => res.json(book));
+  livre
+    .create(newBook)
+    .then(() => res.json({ message: "livre bien ajoutee" }))
+    .catch((e) => {
+      res.status(500).json({ message: "erreur d'ajout" });
+    });
 });
+
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  livre
+    .find({ code: id }, { _id: 0, titre: 1, description: 1, auteur: 1 })
+    .then((data) => res.json(data));
+});
+
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  const { titre, description, auteur } = req.body;
+
+  livre
+    .updateOne(
+      { code: id },
+      { titre: titre, description: description, auteur: auteur }
+    )
+    .then(() => res.json({message: "livre bien modifier"}))
+    .catch((error) => res.status(500).json({ message: 'erreur du modification' }));
+});
+
+router.delete("/:id" , (req,res)=>{
+  const id = req.params.id
+  livre.deleteOne({code:id}).then(()=>res.json({message: "livre supprimer"}))
+  .catch((error)=>res.status(500).json({message:"livre pas trouver"}))
+})
 
 export default router;
