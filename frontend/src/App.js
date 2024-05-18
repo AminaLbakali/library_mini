4f92db6c84f089804c4858d9e059077a64166c9b
AminaLbakali <aminalbakali2023@gmail.com>
2024-05-18 14:21:59 +0100

    added the frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './Navbar';
+import Sidebar from './Sidebar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Sidebar/>
+    </div>
+  );
+}
+
+export default App;
