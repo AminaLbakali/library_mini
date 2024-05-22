8ff2eafcdbf9141a4b6116580a54713d4c271a2a
zakaria <bouzbitazakaria5@.com>
2024-05-22 21:03:39 +0100

    push

--- frontend/src/components/clients/ClientForm.js
+++ frontend/src/components/clients/ClientForm.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate, Link } from 'react-router-dom';
 import { addClient, getClient, updateClient } from '../services/ClientService';
 import Navbar from '../layout/Navbar';
 import './ClientForm.css'; 
@@ -47,40 +47,67 @@ const ClientForm = () => {
     return (
         <> <Navbar />
         <div className="form-container">
-            <h1 className="form-title">{id ? 'Edit' : 'Add'} Client</h1>
-            <form onSubmit={id ? handleUpdateSubmit : handleAddSubmit} className="client-form">
-                <input
-                    type="text"
-                    name="nom"
-                    value={client.nom}
-                    onChange={handleChange}
-                    placeholder="Nom"
-                    required
-                    className="form-input"
-                />
-                <input
-                    type="text"
-                    name="prenom"
-                    value={client.prenom}
-                    onChange={handleChange}
-                    placeholder="Prenom"
-                    required
-                    className="form-input"
-                />
-                <input
-                    type="email"
-                    name="email"
-                    value={client.email}
-                    onChange={handleChange}
-                    placeholder="Email"
-                    required
-                    className="form-input"
-                />
-                <button type="submit" className="form-button">
-                    {id ? 'Update' : 'Add'} Client
-                </button>
-            </form>
+  <h1 className="form-title">{id ? 'Edit' : 'Add'} Client</h1>
+  <form onSubmit={id ? handleUpdateSubmit : handleAddSubmit} className="client-form space-y-12">
+    <div className="border-b border-gray-900/10 pb-12">
+      <div className="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-2">
+        <div className="sm:col-span-2">
+          <label htmlFor="nom" className="block text-sm font-medium leading-6 text-gray-900">Nom</label>
+          <div className="mt-2">
+            <input
+              type="text"
+              name="nom"
+              id="nom"
+              value={client.nom}
+              onChange={handleChange}
+              placeholder="Nom"
+              required
+              className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
         </div>
+
+        <div className="sm:col-span-2">
+          <label htmlFor="prenom" className="block text-sm font-medium leading-6 text-gray-900">Prenom</label>
+          <div className="mt-2">
+            <input
+              type="text"
+              name="prenom"
+              id="prenom"
+value={client.prenom}
+              onChange={handleChange}
+              placeholder="Prenom"
+              required
+              className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+
+        <div className="sm:col-span-2">
+          <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email</label>
+          <div className="mt-2">
+            <input
+              id="email"
+              name="email"
+              type="email"
+              value={client.email}
+              onChange={handleChange}
+              placeholder="Email"
+              required
+              className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="mt-6 flex items-center justify-end gap-x-6">
+      <Link to="/" className="text-sm font-semibold leading-6 text-gray-900">Cancel</Link>
+      <button type="submit" className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+        {id ? 'Update' : 'Add'} Client
+      </button>
+    </div>
+  </form>
+</div>
         </>
     );
 };
--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -1,28 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css'; 
+import logo from './logo.svg'
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="navbar-container">
-        <Link to="/" className="navbar-logo">
-         Book Nook
-        </Link>
-        <ul className="navbar-menu">
-          <li className="navbar-item">
-            <Link to="/" className="navbar-link">
-              Home
-            </Link>
-          </li>
-          <li className="navbar-item">
-            <Link to="/books" className="navbar-link">
-             Books
-            </Link>
-          </li>
-        </ul>
+    <header class="bg-white">
+    <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+      <div class="flex items-center">
+        <div class="flex-shrink-0">
+          <Link to="/">
+          <img class="h-10 w-auto" src={logo} alt="Your Company Name" />
+          </Link>
+        </div>
+        <div class="hidden lg:block lg:ml-6">
+          <h2 class="text-sm font-semibold leading-6 text-gray-900">library</h2>
+        </div>
+      </div>
+      <div class="hidden lg:block lg:ml-6">
+        <div class="flex items-center gap-x-6">
+          <Link to="/books" class="text-sm font-semibold leading-6 text-gray-900">Books</Link>
+          <Link to="/" class="text-sm font-semibold leading-6 text-gray-900">Clients</Link>
+          <Link to="/emprunt" class="text-sm font-semibold leading-6 text-gray-900">Emprunt</Link>
+        </div>
       </div>
     </nav>
+  </header>
   );
 };
 
